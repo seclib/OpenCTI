6480bc9bbd377bb5ccc85f46b866cf4deec3b4d7
Abijah Calderon <168455730+abijahcalderon@users.noreply.github.com>
2024-05-21 14:39:50 -0400

    [frontend] condtionally replaced FAB's for creating investigations and creating/importing dashbaords with regular buttons based on FAB_REPLACED feature flag
    replaced create investigation and create/import dashboard FAB's with regular buttons conditionally based on isFeatureEnable(FAB_REPLACED)
    
    linted
    
    frontend - updated and wrote new tests for creation/updating dashboard and investigations with the new replacement of the floating action buttons

--- opencti-platform/opencti-front/tests_e2e/model/investigations.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/investigations.pageModel.ts
@@ -1,26 +1,26 @@
 import { Page } from '@playwright/test';
 
-export default class DashboardPage {
+export default class InvestigationsPage {
   constructor(private page: Page) {
   }
 
   getPage() {
-    return this.page.getByTestId('dashboard-page');
+    return this.page.getByTestId('investigations-page');
   }
 
   openButtonModal() {
     return this.page.getByLabel('Create', { exact: true });
   }
 
-  addNewDashboard() {
-    return this.page.getByText('Create dashboard', { exact: true });
+  addNewInvestigation() {
+    return this.page.getByText('Create an investigation', { exact: true });
   }
 
-  getDashboardNameInput() {
+  getInvestigationNameInput() {
     return this.page.getByLabel('Name');
   }
 
-  getCreateDashboardButton() {
+  getCreateInvestigationButton() {
     return this.page.getByRole('button', { name: 'Create' });
   }
 
