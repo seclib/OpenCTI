a2cf9581e2cb336c391ad37661a9a463f942cfec
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-06-24 14:39:11 -0400

    [frontend] Genericized EditEntityControlledDial

--- opencti-platform/opencti-front/src/private/components/analyses/Groupings.tsx
+++ opencti-platform/opencti-front/src/private/components/analyses/Groupings.tsx
@@ -27,7 +27,7 @@ interface GroupingsProps {
 const Groupings: FunctionComponent<GroupingsProps> = () => {
   const { t_i18n } = useFormatter();
   const { isFeatureEnable } = useHelper();
-  const FAB_REPLACED = isFeatureEnable('FAB_REPLACEMENT');
+  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
   const {
     platformModuleHelpers: { isRuntimeFieldEnable },
   } = useAuth();
@@ -150,7 +150,7 @@ const Groupings: FunctionComponent<GroupingsProps> = () => {
           paginationOptions={queryPaginationOptions}
           numberOfElements={numberOfElements}
           iconExtension={true}
-          createButton={FAB_REPLACED && <Security needs={[KNOWLEDGE_KNUPDATE]}>
+          createButton={isFABReplaced && <Security needs={[KNOWLEDGE_KNUPDATE]}>
             <GroupingCreation paginationOptions={queryPaginationOptions} />
           </Security>}
         >
@@ -197,7 +197,7 @@ const Groupings: FunctionComponent<GroupingsProps> = () => {
     <ExportContextProvider>
       <Breadcrumbs variant="list" elements={[{ label: t_i18n('Analyses') }, { label: t_i18n('Groupings'), current: true }]} />
       {renderLines()}
-      {!FAB_REPLACED
+      {!isFABReplaced
         && <Security needs={[KNOWLEDGE_KNUPDATE]}>
           <GroupingCreation paginationOptions={queryPaginationOptions} />
         </Security>
--- opencti-platform/opencti-front/src/private/components/analyses/notes/Note.tsx
+++ opencti-platform/opencti-front/src/private/components/analyses/notes/Note.tsx
@@ -72,7 +72,7 @@ const NoteComponent: FunctionComponent<NoteComponentProps> = ({
 }) => {
   const note = useFragment(NoteComponentFragment, noteFragment);
   const { isFeatureEnable } = useHelper();
-  const FABReplaced = isFeatureEnable('FAB_REPLACEMENT');
+  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
 
   return (<>
     <Grid
@@ -127,7 +127,7 @@ const NoteComponent: FunctionComponent<NoteComponentProps> = ({
         <StixCoreObjectLatestHistory stixCoreObjectId={note.id} />
       </Grid>
     </Grid>
-    {!FABReplaced && (
+    {!isFABReplaced && (
       <CollaborativeSecurity data={note} needs={[KNOWLEDGE_KNUPDATE]}>
         <NoteEdition noteId={note.id} />
       </CollaborativeSecurity>
